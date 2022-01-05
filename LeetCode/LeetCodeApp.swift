fb15408f1b5d3c6d8eaedc893e11304c733fd5ab
Devang <papinwardevang@gmail.com>
2022-01-05 17:03:59 +0530

    Initial Commit

---
+++ LeetCode/LeetCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCodeApp.swift
+//  LeetCode
+//
+//  Created by Devang Papinwar on 05/01/22.
+//
+
+import SwiftUI
+
+@main
+struct LeetCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
